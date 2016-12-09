5877330b0fb4ff1a3f39d0c2b106cad474bf9bda
Wouter Thys <thys_wouter@hotmail.com>
2016-12-09 22:48:19 +0100

    moved logic from MainScreen to Manager

--- Manager.py
+++ Manager.py
@@ -1,9 +1,30 @@
 import threading
 import Queue
+
+from pid.PID import PID
 from settings.Settings import Settings
 from my_serial.SerialInterface import SerialInterface
 from gui.MainScreen import MainScreen
 from my_serial.SerialThread import SerialThread
+
+TIME_INTERVAL = 1 # Time interval in seconds
+
+TARGET_Y = []
+TARGET_X = []
+for i in range(0,350):
+    TARGET_X.append(i)
+    if i < 110:
+        TARGET_Y.append((180.0/110.0)*float(i))
+    if (i >= 110) and (i < 200):
+        TARGET_Y.append(180)
+    if (i >= 200) and (i < 250):
+        TARGET_Y.append((120.0/50.0)*(float(i)-200) + 180)
+    if (i >= 250) and (i < 260):
+        TARGET_Y.append(300)
+    if i >= 260:
+        TARGET_Y.append(TARGET_Y[-1]-2)
+
+TARGET = (TARGET_X, TARGET_Y)
 
 
 class Manager:
@@ -21,6 +42,9 @@ class Manager:
         # Create local instances
         self.settings = Settings()
         self.serial = SerialInterface()
+        self.pid = PID(20, 0.005, 0.01)
+        self.pid.dt = TIME_INTERVAL
+        self.cnt = 0
 
         # Create the queue and event
         self.queue = Queue.Queue()
@@ -42,11 +66,35 @@ class Manager:
         Check every 100 ms if there is something new in the queue
         """
         self.gui.process_incoming()
+        self.do_logic()
         if self.event.is_set():
             # This is the brutal stop of the system. Maybe do some cleanup before actually shutting down
             import sys
             sys.exit(1)
         self.master.after(200, self.periodic_call)
+
+    def do_logic(self):
+        while self.queue.qsize():
+            try:
+                msg = self.queue.get(0)
+                if msg.command == "AR":
+                    val = msg.message
+                    try:
+                        val = float(val)
+                        val = self.digital_to_temp(val)
+                        self.pid.set_point = TARGET_Y[self.cnt]  # Point it should be
+                        self.cnt += 1
+                        output = self.pid.do_work(val)
+                        self.gui.graph.update(val, output)
+                    except ValueError:
+                        pass
+                else:
+                    pass
+            except Queue.Empty:
+                pass
+
+    def digital_to_temp(self, value):
+        return value/10
 
     def end_application(self):
         self.serial.serial_destroy()
--- gui/MainScreen.py
+++ gui/MainScreen.py
@@ -31,9 +31,6 @@ class MainScreen:
         self.queue = queue
         self.serial = serial
         self.pic_info = PICInfo
-        self.pid = PID(15, 0.05, 0.001)
-        self.pid.dt = 1
-        self.cnt = 0
         # Set up the main menu
         MainMenu.MainMenu(master, settings, serial, end_command)
         # Window settings
@@ -41,65 +38,16 @@ class MainScreen:
         self.master.wm_title("Oven")
 
         graph_frame = Frame(master)
-        button_frame = Frame(master)
         graph_frame.pack(side=LEFT, fill=BOTH)
-        button_frame.pack(side=RIGHT, fill=BOTH)
 
-        # Buttons
-        self.pic_info_btn = Button(button_frame, text="PIC Info", state=DISABLED, command=self.show_pic_info)
-        self.pic_info_btn.pack(side=TOP, fill=X)
-        #
-        self.pic_awake_btn = Button(button_frame, text="Wake PIC", command=self.send_get_name_request)
-        self.pic_awake_btn.pack(side=TOP, fill=X)
         # Graph
         self.graph = Graph(graph_frame, TARGET)
 
     def process_incoming(self):
         """
-        Handle all the messages currently in the queue (if any)
+        Set the appropriate fields
         """
-        while self.queue.qsize():
-            try:
-                msg = self.queue.get(0)
-                # Check the contents of the message and so something with it
-                #self.status.put_status_message("Message from: "+msg.sender)
-                self.handle_message(msg)
-            except Queue.Empty:
-                pass
+        pass
 
     def show_pic_info(self):
         PicInfoDialog(self.master, self.pic_info)
-
-    def send_analog_read_request(self):
-        self.serial.serial_write("AR", "abc")
-
-    def send_get_name_request(self):
-        self.serial.serial_write("GN", "abc")
-
-    def handle_message(self, message):
-        if message.type == "ack":
-            self.pic_info = message.pic_info
-            self.pic_info_btn["state"] = "normal"
-        else:
-            if message.command == "AR":
-                self.do_logic(message.message)
-            else:
-                pass
-
-    def do_logic(self, val):
-        try:
-            val = float(val)
-            val /= 10
-            self.pid.set_point = TARGET_Y[self.cnt]# Point it should be
-            self.cnt += 1
-            output = self.pid.do_work(val)
-            print output
-
-            self.graph.update(val, output)
-        except ValueError:
-            pass
-
-        pass
-
-
-
